77cda45e91b80708f62f44b745183dddb75ec864
Dariusz Gołąbek <golabiusz@gmail.com>
2021-01-12 21:26:23 +0100

    Slowing down lasers

--- app/src/main/java/com/golabiusz/scrollingshooter/gameobject/PlayerLaserSpec.java
+++ app/src/main/java/com/golabiusz/scrollingshooter/gameobject/PlayerLaserSpec.java
@@ -6,7 +6,7 @@ public class PlayerLaserSpec extends ObjectSpec {
 
   private static final String tag = "Player Laser";
   private static final String bitmapName = "player_laser";
-  private static final float speed = .65f;
+  private static final float speed = 1.5f;
   private static final PointF relativeScale = new PointF(8f, 160f);
   private static final String[] components = new String[]{
       "StdGraphicsComponent",
